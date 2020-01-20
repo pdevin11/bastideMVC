7e27c788ce4ad723b139de78a09a45a98da08b6f
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-01-20 11:09:07 +0100

    update lang choices
    ajout icon fr et en pour les langues

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-default ">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
@@ -11,12 +11,11 @@
             <ul class="nav navbar-nav">
                 <li><a href="index.php?page=home"><?= $lang->header->accueil ?></a></li>
                 <li><a href="index.php?page=carte"><?= $lang->header->carte ?></a></li>
-                <li><a href="index.php?page=actualite"><?= $lang->header->actu ?></a></li>
                 <li><a href="index.php?page=photos"><?= $lang->header->photos ?></a></li>
                 <li><a href="index.php?page=reservation"><?= $lang->header->reservation ?></a></li>
                 <li><a href="index.php?page=contact"><?= $lang->header->contact ?></a></li>
-                <li><a href="index.php?page=home&lang=fr">Fr</a></li>
-                <li><a href="index.php?page=home&lang=en">En</a></li>
+                <li><a href="index.php?page=home&lang=fr"><img src='assets/images/icon-fr.png' alt="icone fr" class="lang-icon"></a></li>
+                <li><a href="index.php?page=home&lang=en"><img src='assets/images/icon-en.png' alt="icone en" class="lang-icon"></a></li>
             </ul>
         </div>
     </nav>
