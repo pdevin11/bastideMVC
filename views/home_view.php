10ee7cdc96220fb49b77b36305dadfb10e62a064
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-01-22 11:46:14 +0100

    delete menu_images, reviews, uptade description
    Suppression des "images menu", du carousel des avis.
    Ajout d'une phrase-liens dans la description

--- views/home_view.php
+++ views/home_view.php
@@ -20,6 +20,7 @@
         <section id="restaurant_description">
             <h1>Restaurant La Bastide</h1>
             <p><?= $lang->home->presentation ?></p>
+            <p><?= $lang->home->carte_resa ?></p>
 
             <h4><?= $lang->home->horaires ?></h4>
             <p><?= $lang->home->horairesDetails ?></p>
@@ -28,34 +29,6 @@
             <p><?= $lang->home->paiementDetails ?></p>
 
         </section><!-- /#restaurant_description -->
-
-    <!-- Images cliquables -->
-
-        <div class="menu_frame">
-            <a href="index.php?page=actualite" class="links">
-                <div id="actu" class="div_fade">
-                    <p id="actu_link"><?= $lang->home->actualites ?></p>
-                </div><!-- /#actu -->
-            </a><!-- /.links -->
-
-            <a href="index.php?page=carte" class="links">
-                <div id="carte" class="div_fade">
-                    <p id="carte_link"><?= $lang->home->carte ?></p>
-                </div><!-- /#carte -->
-            </a><!-- /.links -->
-
-            <a href="index.php?page=contact" class="links">
-                <div id="contact" class="div_fade">
-                    <p id="contact_link"><?= $lang->home->contact ?></p>
-                </div><!-- /#contact -->
-            </a><!-- /.links -->
-
-        </div><!-- /.menu_frame -->
-    
-    <!-- carousel des avis -->
-
-        <!-- code dans le fichier script nommé carrousselHome.php à retranscrire apres création des classes et du controller -->
-        
         
     </div><!-- /.container -->
     
