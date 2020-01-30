6ffa0a25ac75389439072bdbe2af24d30aaf9daf
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-01-30 16:40:26 +0100

    add admin_plat_update.php

---
+++ controllers/admin_plat_update_controller.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once '_classes/Carte.php';
+include_once '_classes/Admin.php';
+
+/* Modification d'un plat */
+
+$id = str_secur($_GET['id']);
+
+$dish = Carte::getDish($id);
+
+if(isset($_POST['modifier'])){
+    if(empty($_POST['plat_name'])){
+        $plat_nameError = 'Vous devez saisir un nom pour le plat';
+    }else{
+        $name = str_secur($_POST['plat_name']);
+    }
+
+    if(empty($_POST['translation'])){
+        $translationError = 'Vous devez saisir une traduction pour le nom du plat';
+    }else{
+        $translation = str_secur($_POST['translation']);
+    }
+
+    if(empty($_POST['plat_type'])){
+        $plat_typeError = 'Vous devez indiquer le type de plat';
+    }else{
+        $type = str_secur($_POST['plat_type']);
+    }
+
+    if(empty($_POST['plat_price'])){
+        $plat_priceError = 'Vous devez indiquer le prix du plat';
+    }else{
+        $price = str_secur($_POST['plat_price']);
+    }
+
+    if(!empty($_POST['bigSize_price'])){
+        $bigSize = str_secur($_POST['bigSize_price']);
+    }
+
+    if(isset($name, $translation, $type, $price, $bigSize)){
+        Admin::updatePlat($name, $translation, $type, $price, $bigSize, $id);
+        header('location:index.php?page=admin_menu');
+    } else if (isset($name, $translation, $type, $price) && empty($bigSize)){
+        Admin::updatePlat($name, $translation, $type, $price, NULL, $id);
+        header('location:index.php?page=admin_menu');
+    }
+}
