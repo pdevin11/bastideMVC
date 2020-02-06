65552b92c710a02bdce6176b7a3e35bb78225424
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-02-06 09:47:37 +0100

    upload admin_menu_view.php
    modification des attributs des div tab-pane

--- views/admin_menu_view.php
+++ views/admin_menu_view.php
@@ -1,18 +1,18 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <?php include_once 'includes/adminHead.php'; ?>
-    <title><?= ucfirst($page) ?> - Restaurant la Bastide</title>
+    <?php include_once 'includes/adminHead.php';?>
+    <title><?=ucfirst($page)?> - Restaurant la Bastide</title>
 </head>
 
 <body>
-<div class="container">
+    <div class='container'>
+    
+    <h1 class="admin_h1">Panneau d'administration</h1>
 
-<h1 class="admin_h1">Panneau d'administration</h1>
+    <!-- Header: Menu principal -->
 
-<!-- Header : menu principal -->
-
-<?php include_once 'includes/adminHeader.php'; ?>
+    <?php include_once 'includes/adminHeader.php'; ?>
 
     <div class='row'>
 
@@ -26,83 +26,113 @@
         </div> <!-- /.col-md-3 -->
 
         <div class='col-md-9'>
+        
             <div class='tab-content'>
 
-                <div <?php if(isset($_POST['btn-name'])){echo 'class="tab-pane active"';} elseif(isset($_POST['btn-firstname']) || isset($_POST['btn-city'])){echo 'class="tab-pane"';}else{echo 'class="tab-pane active"';} ?> id='1'>
-                    <h2>Gérer les menus <a href="index.php?page=admin_menu_insert"><button type="button" class="btn btn-outline-success">Ajouter</button></a></h2>
+                <div class="tab-pane active" id='1'>
 
-                    <table class="table table-striped">
-                        <thead class="thead-dark">
-                            <tr>
-                            <th scope="col">#</th>
+                <h2>Gérer les menus <a href="index.php?page=admin_menu_insert"><button type="button" class="btn btn-outline-success">Ajouter</button></a></h2>
+
+                <br><br>
+
+                <h3>Menus en ligne</h3>
+
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
                             <th scope="col">Nom</th>
                             <th scope="col">Prix</th>
                             <th scope="col">Modifier</th>
-                            <th scope="col">Supprimer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                            <?php foreach($allMenus as $menu){ ?>
+                            <th scope="col">Passer Hors ligne</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <?php foreach($menusOnline as $menu){ ?>
 
-                            <tr>
-                                <th scope="row"><?= $menu['id']?></th>
-                                <td><?=$menu['menu_name']?></td>
-                                <td><?=$menu['menu_price']?></td>
-                                <td><a href="index.php?page=admin_menu_update&id=<?=$menu['id']?>"><button type="button" class="btn btn-outline-warning">Mofidier</button></a></td>
-                                <td><a href="index.php?page=admin_menu_delete&id=<?=$menu['id']?>"><button type="button" class="btn btn-outline-danger">Supprimer</button></a></td>
-                            </tr>
-                            
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                    
+                        <tr>
+                            <td scope="row"><?=$menu['menu_name']?></td>
+                            <td><?=$menu['menu_price']?></td>
+                            <td><a href="index.php?page=admin_menu_update&id=<?=$menu['id']?>"><button type="button" class="btn btn-outline-warning">Mofidier</button></a></td>
+                            <td><a href="index.php?page=admin_menu_delete&id=<?=$menu['id']?>"><button type="button" class="btn btn-outline-danger">Passer Hors ligne</button></a></td>
+                        </tr>
+                        
+                        <?php } ?>
+                    </tbody>
+                </table>
+
+                <br><br>
+
+                <h3>Menus enregistrés</h3>
+
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Prix</th>
+                            <th scope="col">Modifier / Mettre en ligne</th>
+                            <th scope="col">Supprimer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <?php foreach($menusPending as $menuP){ ?>
+
+                        <tr>
+                            <td scope="row"><?=$menuP['menu_name']?></td>
+                            <td><?=$menuP['menu_price']?></td>
+                            <td><a href="index.php?page=admin_menu_update&id=<?=$menuP['id']?>"><button type="button" class="btn btn-outline-warning">Mofidier / Mettre en ligne</button></a></td>
+                            <td><a href="index.php?page=admin_menu_delete&id=<?=$menuP['id']?>"><button type="button" class="btn btn-outline-danger">Supprimer</button></a></td>
+                        </tr>
+                        
+                        <?php } ?>
+                    </tbody>
+                </table>
+                
+
                 </div> <!-- /.tab-pane #1 -->
 
-                <div <?php if(isset($_POST['btn-firstname'])){echo 'class="tab-pane active"';}else{echo 'class="tab-pane"';} ?> id='2'>
+                <div class="tab-pane" id='2'>
+
                 <h2>Gérer les plats à la carte <a href="index.php?page=admin_plat_insert"><button type="button" class="btn btn-outline-success">Ajouter</button></a></h2>
 
-                    <table class="table table-striped">
-                        <thead class="thead-dark">
-                            <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nom</th>
-                            <th scope="col">Type</th>
-                            <th scope="col">Prix</th>
-                            <th scope="col">Prix XL</th>
-                            <th scope="col">Modifier</th>
-                            <th scope="col">Supprimer</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                            <?php foreach($allCartePlats as $cartePlat){ ?>
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col">Nom (Traduction)</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Prix</th>
+                        <th scope="col">Prix XL</th>
+                        <th scope="col">Modifier</th>
+                        <th scope="col">Supprimer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <?php foreach($allCartePlats as $cartePlat){ ?>
 
-                            <tr>
-                                <th scope="row"><?= $cartePlat['id']?></th>
-                                <td><?=$cartePlat['plat_name']?></td>
-                                <td><?=$cartePlat['plat_type']?></td>
-                                <td><?=$cartePlat['price']?> €</td>
-                                <td><?php if(isset($cartePlat['bigSize_price'])){echo $cartePlat['bigSize_price'].'€';}?></td>
-                                <td><a href="index.php?page=admin_plat_update&id=<?=$cartePlat['id']?>"><button type="button" class="btn btn-outline-warning">Mofidier</button></a></td>
-                                <td><a href="index.php?page=admin_plat_delete&id=<?=$cartePlat['id']?>"><button type="button" class="btn btn-outline-danger">Supprimer</button></a></td>
-                            </tr>
-                            
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                        <tr>
+                            <td scope="row"><?=$cartePlat['plat_name']?> <br> ( <?=$cartePlat['translation']?> )</td>
+                            <td><?=$cartePlat['plat_type']?></td>
+                            <td><?=$cartePlat['price']?> €</td>
+                            <td><?php if(isset($cartePlat['bigSize_price'])){echo $cartePlat['bigSize_price'].'€';}?></td>
+                            <td><a href="index.php?page=admin_plat_update&id=<?=$cartePlat['id']?>"><button type="button" class="btn btn-outline-warning">Mofidier</button></a></td>
+                            <td><a href="index.php?page=admin_plat_delete&id=<?=$cartePlat['id']?>"><button type="button" class="btn btn-outline-danger">Supprimer</button></a></td>
+                        </tr>
+                        
+                        <?php } ?>
+                    </tbody>
+                </table>
+                
+
                 </div> <!-- /.tab-pane #2 -->
-
+            
             </div> <!-- /.tab-content -->
-        </div> <!-- /.col-md-9 -->
 
+        </div> <!-- /.col-md-9 -->
+    
     </div> <!-- /.row -->
 
-</div> <!-- /.container -->
-
-<!-- Footer -->
-
-<?php include_once 'includes/adminFooter.php'; ?>
-
-</body>    
+    </div> <!-- /.container -->
+</body>
 </html>
