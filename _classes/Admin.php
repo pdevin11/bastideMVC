bbc8fac7035f423cd04f7090b9161010dcd098fc
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-02-06 10:13:24 +0100

    update Admin.php
    Ajout de méthodes
    setMenuOnline et setMenuOffline
    permettent de passer respectivement un menu en ligne et hors ligne en modifiant la colone "isOnline" de la BDD qui vaut false par défaut. Utilisation de la méthode setMenuOnline pour passer le menu en ligne qui modifie la valeur de "isOnline" à true

--- _classes/Admin.php
+++ _classes/Admin.php
@@ -1,17 +1,33 @@
 <?php
 
 class Admin{
-    
+
+    /* Gestion des menus */
+
     /**
-     * Ajoute un nouveau menu dans la base de donnée
+     * Ajoute un nouveau menu en BDD dont id en AI et isOnline initialisé à false par défaut
      * @param $name, $price
-     * @return $addMenu
+     * @return array
      */
     static function addMenu($name, $price){
         global $db;
 
         $addMenu = $db->prepare('INSERT INTO menus (menu_name, menu_price) VALUES (?, ?)');
         return $addMenu->execute(array($name, $price));
+    }
+
+    static function setMenuOnline($id){
+        global $db;
+
+        $setOnline = $db->prepare('UPDATE menus SET isOnline = "true" WHERE id = ?');
+        return $setOnline->execute(array($id));
+    }
+
+    static function setMenuOffline($id){
+        global $db;
+
+        $setOffline = $db->prepare('UPDATE menus SET isOnline = "false" WHERE id = ?');
+        return $setOffline->execute(array($id));
     }
 
     /**
@@ -24,7 +40,6 @@ class Admin{
 
         $updateMenu = $db->prepare('UPDATE menus SET menu_name = ?, menu_price = ? WHERE id = ?');
         return $updateMenu->execute(array($name, $price, $id));
-
     }
 
     /**
@@ -37,8 +52,47 @@ class Admin{
 
         $deleteMenu = $db->prepare('DELETE FROM menus WHERE id = ?');
         return $deleteMenu->execute(array($id));
-
     }
+
+    /* Gestion des plats d'un menu */
+
+    /**
+     * Ajoute un plat à un menu en fonction de l'id du menu
+     * @param $name, $translation, $type, $menuName, $idMenu
+     * @return array
+     */
+    static function addPlatToMenu($name, $translation, $type, $menuName, $idMenu){
+        global $db;
+
+        $addPTM = $db->prepare('INSERT INTO plats_names(plat_name, translation, plat_type, menu_name, id_menu) VALUES (?, ?, ?, ?, ?)');
+        return $addPTM->execute(array($name, $translation, $type, $menuName, $idMenu));
+    }
+
+    /**
+     * Modifie un plat d'un menu en fonction de l'id du plat
+     * @param $name, $translation, $type, $id
+     * @return array
+     */
+    static function updatePlatToMenu($name, $translation, $type, $id){
+        global $db;
+
+        $updatePTM = $db->prepare('UPDATE plats_names SET plat_name = ?, translation = ?, plat_type = ? WHERE id = ?');
+        return $updatePTM->execute(array($name, $translation, $type, $id));
+    }
+
+    /**
+     * Supprime un plat d'un menu en fonction de l'id du plat
+     * @param $id
+     * @return array
+     */
+    static function deletePlatToMenu($id){
+        global $db;
+
+        $deletePTM = $db->prepare('DELETE FROM plats_names WHERE id = ?');
+        return $deletePTM->execute(array($id));
+    }
+
+    /* Gestion de la carte des plats */
 
     /**
      * Ajoute un nouveau plat
@@ -62,7 +116,6 @@ class Admin{
 
         $updatePlat = $db->prepare('UPDATE menu_of_dishes SET plat_name = ?, translation = ?, plat_type = ?, price = ?, bigSize_price = ? WHERE id = ?');
         return $updatePlat->execute(array($name, $translation, $type, $price, $bigSize, $id));
-
     }
 
     /**
@@ -75,6 +128,5 @@ class Admin{
 
         $deletePlat = $db->prepare('DELETE FROM menu_of_dishes WHERE id = ?');
         return $deletePlat->execute(array($id));
-
     }
 }
