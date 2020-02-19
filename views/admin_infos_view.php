7d1c5fa46f1173c723510d5f428dd1930b72eb45
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-02-19 21:21:14 +0100

    update admin_infos et home_view.php
    admin_infos : Ajout de la fonctionalité de gestion des ouvertures et fermetures du restaurant
    home : Ajout du tableau des ouvertures

--- views/admin_infos_view.php
+++ views/admin_infos_view.php
@@ -14,10 +14,218 @@
 
         <?php include_once 'includes/adminHeader.php'; ?>
 
-    <h2>INFOS</h2>
+    <h2>Gestion des horaires</h2>
 
-        
-    </div><!-- /.container -->
+    <br>
+
+    <form action="" method='post'>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8" id='table'>
+                
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Midi ?</th>
+                            <th scope="col">Actuellement le midi</th>
+                            <th scope="col">Soir ?</th>
+                            <th scope="col">Actuellement le soir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <td scope="row"><h4>Lundi</h4></td>
+                            <td>
+                                <?php if($openings[0]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='monLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='monLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[0]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[0]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='monDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='monDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[0]['isOpenForDiner'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Lundi -->
+
+                        <tr>
+                            <td scope="row"><h4>Mardi</h4></td>
+                            <td>
+                                <?php if($openings[1]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='tueLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='tueLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[1]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[1]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='tueDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='tueDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[1]['isOpenForDiner'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Mardi -->
+
+                        <tr>
+                            <td scope="row"><h4>Mercredi</h4></td>
+                            <td>
+                                <?php if($openings[2]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='wedLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='wedLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[2]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[2]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='wedDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='wedDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[2]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Mercredi -->
+
+                        <tr>
+                            <td scope="row"><h4>Jeudi</h4></td>
+                            <td>
+                                <?php if($openings[3]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='thurLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='thurLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[3]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[3]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='thurDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='thurDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[3]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Jeudi -->
+
+                        <tr>
+                            <td scope="row"><h4>Vendredi</h4></td>
+                            <td>
+                                <?php if($openings[4]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='friLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='friLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[4]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[4]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='friDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='friDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[4]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Vendredi -->
+
+                        <tr>
+                            <td scope="row"><h4>Samedi</h4></td>
+                            <td>
+                                <?php if($openings[5]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='satLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='satLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[5]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[5]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='satDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='satDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[5]['isOpenForDiner'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Samedi -->
+
+                        <tr>
+                            <td scope="row"><h4>Dimanche</h4></td>
+                            <td>
+                                <?php if($openings[6]['isOpenForLunch'] == 1){ ?>
+                                    <input type="submit" name='sunLunchClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='sunLunchOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[6]['isOpenForLunch'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                            <td>
+                                <?php if($openings[6]['isOpenForDiner'] == 1){ ?>
+                                    <input type="submit" name='sunDinerClosed' class='btn btn-danger' value='Fermer'>
+                                <?php } else { ?>
+                                    <input type="submit" name='sunDinerOpen' class='btn btn-success' value='Ouvrir'>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if($openings[6]['isOpenForDiner'] == 1){echo '<b>Ouvert</b>';} 
+                                else {echo '<b>Fermé</b>';}?>
+                            </td>
+                        </tr> <!-- /tr Dimanche -->
+                        
+                    </tbody>
+                </table>
+
+            </div> <!-- /.col-md-8 -->
+            <div class="col-md-2"></div>
+        </div> <!-- /.row -->
+    </form>
+    
+    </div> <!-- /.container -->
     
     <!-- Footer -->
 
--- views/home_view.php
+++ views/home_view.php
@@ -23,7 +23,166 @@
             <p><?= $lang->home->carte_resa ?></p>
 
             <h4><?= $lang->home->horaires ?></h4>
-            <p><?= $lang->home->horairesDetails ?></p>
+            <p><?php /* $lang->home->horairesDetails */ ?></p>
+            
+            <div class='row'>
+                <div class='col-md-3'></div>
+                <div class='col-md-6'>
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col"><?= $lang->home->midi?></th> 
+                            <th scope="col"><?= $lang->home->soir?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <!-- Lundi -->
+
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->mon?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[0]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[0]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Lundi -->
+
+                        <!-- Mardi -->
+
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->tue?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[1]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[1]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Mardi -->
+
+                        <!-- Mercredi -->
+                    
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->wed?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[2]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[2]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Mercredi -->
+
+                        <!-- Jeudi -->
+
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->thur?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[3]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[3]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Jeudi -->
+
+                        <!-- Vendredi -->
+
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->fri?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[4]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[4]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Vendredi -->
+
+                        <!-- Samedi -->
+
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->sat?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[5]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[5]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Samedi -->
+
+                        <!-- Dimanche -->
+
+                        <tr>
+                            <td scope="row">
+                                <?=$lang->home->sun?>
+                            </td>
+                            <td>
+                                <?php 
+                                    if($infos[6]['isOpenForLunch'] == 1){echo $lang->home->open;} 
+                                    else {echo $lang->home->closed;} 
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    if($infos[6]['isOpenForDiner'] == 1){echo $lang->home->open;}
+                                    else {echo $lang->home->closed;}
+                                ?>
+                            </td> 
+                        </tr> <!-- /tr Dimanche -->
+
+                    </tbody>
+                </table>
+                </div>
+                <div class='col-md-3'></div>
+            </div>
 
             <h4><?= $lang->home->paiement ?></h4>
             <p><?= $lang->home->paiementDetails ?></p>
