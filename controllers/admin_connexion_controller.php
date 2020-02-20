653e9a3ba45bfa46de645567e5e0dc778689366a
pdevin11 <51492359+pdevin11@users.noreply.github.com>
2020-02-20 21:07:10 +0100

    Update admin_connexion_controller.php
    Gestion de la connexion à l'administration du site

---
+++ controllers/admin_connexion_controller.php
@@ -0,0 +1,33 @@
+<?php session_start(); 
+
+if(!isset($_SESSION['identifiant'])){
+    header('location:index.php?page=admin_connexion');
+}
+
+if(isset($_POST['connexion'])){
+
+    if(isset($_POST['identifiant'])){
+
+        $identifiant = str_secur($_POST['identifiant']);
+        $inDbPass = Users::getPassword($identifiant);
+
+        if(isset($_POST['password'])){
+
+            $password = str_secur($_POST['password']);
+            $isCorrect = password_verify($password, $inDbPass['passwordHash']);
+
+            if(!$inDbPass){
+                $passwordError = 'Identifiant ou mot de passe incorrect';
+            } else {
+                session_start();
+                $_SESSION['identifiant'] = $identifiant;
+                header('location:index.php?page=admin_home');
+            }
+        } else {
+            $passwordError = 'Vous devez saisir un mot de passe pour vous connecter';
+        }
+
+    } else {
+        $identifiantError = 'Vous devez saisir un identifiant pour vous connecter.';
+    }
+}
